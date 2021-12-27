30ca0ff7e878214c137039d4c0cde5e5f971d198
DILILLO NICOLA <s284963@po.polito.it>
2021-12-27 12:13:40 +0100

    BIST controller

--- LBIST/BIST_controller.vhd
+++ LBIST/BIST_controller.vhd
@@ -1,38 +1,52 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_textio.all;
+use ieee.Numeric_Std.all;
+
 
 entity LBIST is
 	generic (
-		SEED_dimension : integer := 32;
+		SEED_dimension   : integer := 32;
+		SEED_number	     : integer := 10;
+		LOOP_for_SEED    : integer := 200;
 		SCAN_CHAIN_number: integer := 190;
+		MISR_size		 : integer := 32;
+		MISR_value_gold  : integer := 0
 	);
 	port(
 		CLK: IN std_logic;
-		RESET: IN std_logic;
+		RST: IN std_logic;
 		START: IN std_logic;
-		TEST_NORMAL: IN std_logic; -- 1 = test || 0 = normal
+        MISR_value: IN std_logic_vector(MISR_size-1 DOWNTO 0); -- see MISR FF values
+        MISR_enable: OUT std_logic;   -- enable shift inside FF of the MISR
+		TEST_en_i_s: OUT std_logic;   -- put riscv processor in 1 = test || 0 = normal
+		TEST_mode: OUT std_logic;     -- mux for test point
 		MISR_Pass_Fail: IN std_logic; -- 1 = PASS || 0 = fail
-		GO_NOGO: OUT std_logic; -- 1 = GO || 0 = No Go
+		GO_NOGO: OUT std_logic; 	  -- 1 = GO || 0 = No Go
 		LFSR_reset : OUT std_logic;
-		DUT_reset : OUT std_logic;
-		seed : OUT std_logic_vector(SEED_dimension-1 downto 0);
-		TEST_en_i_s: OUT std_logic
+		MISR_reset : OUT std_logic;
+		seed: OUT std_logic_vector(SEED_dimension-1 downto 0) -- for lfsr
 	);
 end entity;
 
 
 architecture beh of LBIST is
 
-	type state is (RESET, SEED_injection_state, SCAN_Propagation_state, END_SCAN_state, Testing, End_state);
-	signal current_state, next_state : state;
-	signal CNT_CLK: integer := 0;
-	signal CNT_LOOP: integer := 0;
-	type seed_array is array (natural range <>) of std_logic_vector(SEED_dimension -1 downto 0);
+	type state is (
+		RESET, 
+		SEED_injection_state,
+		SCAN_Propagation_state,
+		SCAN_normal,
+		END_SCAN_state_1, 
+		END_SCAN_state_2,
+		END_good);
+	signal Current_state, Next_state: state := RESET;
+	signal Cnt_seed, Cnt_seed_next: unsigned(9 downto 0); -- count seed changed
+	signal Cnt_loop, Cnt_loop_next: unsigned(9 downto 0); -- count how many loop for each seed
+	signal Seed_index, Seed_index_next: unsigned(9 downto 0); -- count how many loop for each seed
+	signal seed_s, seed_next_s: std_logic_vector(SEED_dimension-1 downto 0);
+	-- ROM memory for lfsr seed
+	type seed_array is array (natural range <>) of std_logic_vector(SEED_dimension-1 downto 0);
 	signal seed_mem : seed_array(0 to 9);
-	signal seed_index : integer range 0 to 9;
-	signal START_LOOP: std_logic := '0';
 	constant seed_mem_INIT : seed_array(0 to 9) := 
 	(	
 		"0000010110101000000110001",
@@ -48,89 +62,100 @@ architecture beh of LBIST is
 							
 	);
 
-
-
 begin
--- Load the SEED memory and set the initial value for current_state
-seed_mem <= seed_mem_INIT;
-current_state <= RESET;
--- This process is used to count the clock cycles and to update the state
-CLK_proc: process(CLK)
+
+	-- This process is used to count the clock cycles and to update the state
+	CLK_proc: process(CLK)
 	begin
 		if (rising_edge(CLK)) then
-			current_state <= next_state;
-			if( START_LOOP = 1 ) then 
-				CNT_CLK := CNT_CLK + 1;
+			if (RST = '1') then
+				current_state <= RESET;
+				Cnt_seed <= (others => '0');
+				Cnt_loop <= (others => '0');
+				Seed_index <= (others => '0');
+				seed_s <= (others => '0');
+			else 
+				current_state <= Next_state;
+				Cnt_seed <= Cnt_seed_next;
+				Cnt_loop <= Cnt_loop_next;
+				Seed_index <= Seed_index_next;
+				seed_s <= seed_next_s;
 			end if;
 		end if;
 	end process;
 
 
+    seed <= seed_s;
 
-
-FSM: process (current_state)
+FSM: process (current_state, cnt_loop, Cnt_seed, Seed_index, start, seed_s)
 	begin
+		LFSR_reset <= '0';
+		MISR_reset <= '0';
+		TEST_en_i_s <= '0';
+		TEST_mode <= '1';
+		GO_NOGO <= '0';
+
 		case current_state is
 			when RESET => -- In reset all the signals are set to their reset value
-				DUT_reset <= '0';
-				LFSR_reset <= '1';
-				seed_index := 0;
-				CNT_CLK := 0;
-				CNT_LOOP := 0;
-			when SEED_injection_state => --Here we reset the LFSR and inject the new seed
-				seed <= seed_mem (seed_index);
-				LFSR_reset <= '1';
-			when SCAN_propagation_state => -- In this state we start the propagation of the input for 190 clock cycles
-				START_LOOP <= '1';
-				DUT_reset <= '1';
-				LFSR_reset <= '0';
-				TEST_en_i_s <= '1';
-				CNT_LOOP := CNT_LOOP + 1;
-			when END_SCAN_state => -- Here we take the value from the MISR 
-				START_LOOP <= '0'; --We set START_LOOP to 0 in order to stop the count for the loop 
-				CNT_CLK := 0; 		-- Non sono sicuro di questo passaggio qua, il MISR ha bisogno di 16 colpi di clock per dare un uscita in quanto abbiamo 16 FF
-				TEST_en_i_s <= '0'; -- Forse è da modificare aggiungendo questa cosa?
-			when Testing => -- Here we check the result of the MISR in case it's a fail, everything is stopped otherwise we increase the seed_index and restart
-				if (MISR_Pass_Fail = '0') then
-					GO_NOGO <= '0';
+				TEST_mode <= '0';
+				Cnt_seed_next <= (others => '0');
+				Cnt_loop_next <= (others => '0');
+				Seed_index_next <= (others => '0');
+				seed_next_s <= (others => '0');
+				if (start = '1') then
+					Next_state <= SEED_injection_state;
 				else
-					GO_NOGO <= '1';
-					seed_index := seed_index +1;
+					Next_state <= RESET;
 				end if;
-			when END_state =>
-				--Qua non so cosa mettere quando finisce tutti i test di tutto
-				
-	end process;
 
-State_proc: process(CNT_CLK, CNT_LOOP, current_state, START)
-	begin
-	case current_state is
-		when RESET =>
-			if (START = '1') then
-				next_state <= SEED_injection_state;	-- When we receive the START signal we set the SEED_injection
-			end if;
-		when SEED_injection_state =>
-				next_state <= SCAN_propagation_state; --right after the seed has been injected we start the loop
-		when SCAN_propagation_state => -- we stay in this state until all the 190 clock cycles have been executed
-			if(CNT_CLK = SCAN_CHAIN_number) then
-				next_state <= END_SCAN_state;
-			end if;
-		when END_SCAN_state => --in case we finished the loop we check the result of the MISR, otherwise we start the loop again
-			if(CNT_LOOP = 100) then
-				next_state <= Testing;
-			else
-				next_state <= SCAN_propagation_state;
-			end if;
-		when Testing => --in case we have a FAIL from the MISR or we finished all our seeds, we exit, otherwise we set another seed and start again
-			if(MISR_Pass_Fail = '0') then
-				next_state <= END_state;
-			elsif (seed_index < 10) then
-				next_ state <= SEED_injection_state;
-			else 
-				next_state <= END_state;
-			end if;
-	end process;
+			-- Here we reset the LFSR and inject the new seed
+			when SEED_injection_state =>
+			    seed_next_s <= seed_mem (to_integer(seed_index));
+				LFSR_reset <= '1';
+				MISR_reset <= '1';
+				Next_state <= SCAN_propagation_state;
 
+			-- In this state we start the propagation of the input for 190 clock cycles
+			when SCAN_propagation_state =>
+				TEST_en_i_s <= '1';
+				Cnt_loop_next <= Cnt_loop + 1;
+				if (Cnt_loop = SCAN_CHAIN_number - 2) then
+					Next_state <= SCAN_normal;
+				else
+					Next_state <= SCAN_propagation_state;
+				end if;
+
+			-- in this state we run in normale mode to capture
+			when SCAN_normal =>
+				Cnt_seed_next <= Cnt_seed + 1;
+				if (Cnt_seed = SEED_number - 2) then
+					Next_state <= END_SCAN_state_1;
+				else
+					Next_state <= SEED_injection_state;
+				end if;
+
+			-- end the bist, start check the result
+			when END_SCAN_state_1 => -- Here we take the value from the MISR
+				TEST_mode <= '0';
+				GO_NOGO <= '1'; -- first one say as that the procedure is finish
+
+			when END_SCAN_state_2 => -- Here we check the result of the MISR in case it's a fail, everything is stopped otherwise we increase the seed_index and restart
+				if (to_integer(unsigned(MISR_value)) = MISR_value_gold) then
+					Next_state <= END_good;
+				else 
+					Next_state <= RESET;
+				end if;
+
+			-- if reach this state the bist procedure has prdce no failure
+			when END_good =>
+			    GO_NOGO <= '1';
+				Next_state <= RESET;
+
+			when others =>
+				Next_state <= RESET;	
+		
+		end case;
+	end process;
 
 end beh;
 
